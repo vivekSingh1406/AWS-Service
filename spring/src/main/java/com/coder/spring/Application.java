0ad8b8c16ac6d66e48090d61d90c8d2f84c2a218
vivek-singh <139918328+vivekSingh1406@users.noreply.github.com>
2025-07-12 00:28:51 +0530

    Update Application.java

--- spring/src/main/java/com/coder/spring/Application.java
+++ spring/src/main/java/com/coder/spring/Application.java
@@ -14,3 +14,11 @@ public class Application {
 	}
 
 }
+/*
+OAuth2: A widely used authorization framework that allows third-party applications to access user 
+data without sharing credentials. It supports various flows (e.g., authorization code, client credentials).
+
+JWT (JSON Web Tokens): A compact, URL-safe means of representing claims to be transferred between two 
+parties. It is often used for stateless authentication.
+
+*/	
