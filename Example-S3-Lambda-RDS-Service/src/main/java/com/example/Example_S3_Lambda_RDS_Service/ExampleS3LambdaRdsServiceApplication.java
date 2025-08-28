a908e34b461b84963a241bf4a159ac429bacc619
viveksingh1406 <viveksb1207@gmail.com>
2025-08-28 22:51:25 +0530

    add s3, lambda amd rds aws service, to connect

---
+++ Example-S3-Lambda-RDS-Service/src/main/java/com/example/Example_S3_Lambda_RDS_Service/ExampleS3LambdaRdsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Example_S3_Lambda_RDS_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleS3LambdaRdsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleS3LambdaRdsServiceApplication.class, args);
+	}
+
+}
